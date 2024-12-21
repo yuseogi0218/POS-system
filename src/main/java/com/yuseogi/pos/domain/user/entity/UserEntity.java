1ca9bf6b9cb96d9ddd474451441bfa3aba335fa6
yuseogi0218 <yuseogi0218@gmail.com>
2024-12-21 14:13:15 +0900

    #1 FEAT - User 엔티티

---
+++ src/main/java/com/yuseogi/pos/domain/user/entity/UserEntity.java
@@ -0,0 +1,71 @@
+package com.yuseogi.pos.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Table(name = "UserTable")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", nullable = false, length = 10)
+    private String name;
+
+    @Column(name = "phone", nullable = false, length = 11)
+    private String phone;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<SimpleGrantedAuthority> response = new ArrayList<>();
+        response.add(new SimpleGrantedAuthority(email));
+
+        return response;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
