28f1fb3a0e3b9268611bed0544dc26fa1d6bf1cf
yuseogi0218 <yuseogi0218@gmail.com>
2024-12-31 02:39:16 +0900

    #28 FEAT - 각 Micro Service Docker File 생성

--- gateway-service/src/main/java/com/yuseogi/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/yuseogi/gatewayservice/GatewayServiceApplication.java
@@ -3,7 +3,7 @@ package com.yuseogi.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.yuseogi.gatewayservice"})
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
