2258efff2aa195b3a63d0ca6e2a19b8a97c9a17a
yuseogi0218 <yuseogi0218@gmail.com>
2025-01-02 13:59:54 +0900

    FEAT - batch server 프로젝트 생성 및 초기화

---
+++ batch-server/src/main/java/com/yuseogi/batchserver/BatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.yuseogi.batchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServerApplication.class, args);
+	}
+
+}
